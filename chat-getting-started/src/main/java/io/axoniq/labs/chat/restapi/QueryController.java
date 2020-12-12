a0fbd17e56872604793d012f8d4c514bc39e9624
Berendea Nicolae <berendeanicolae@gmail.com>
2020-12-12 18:46:22 +0100

    query interface

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/restapi/QueryController.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/restapi/QueryController.java
@@ -1,8 +1,12 @@
 package io.axoniq.labs.chat.restapi;
 
+import io.axoniq.labs.chat.coreapi.*;
+
 import io.axoniq.labs.chat.query.rooms.messages.ChatMessage;
 import io.axoniq.labs.chat.query.rooms.summary.RoomSummary;
+import org.axonframework.messaging.responsetypes.MultipleInstancesResponseType;
 import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +15,9 @@ import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.concurrent.Future;
+
+import static org.axonframework.messaging.responsetypes.ResponseTypes.instanceOf;
+import static org.axonframework.messaging.responsetypes.ResponseTypes.multipleInstancesOf;
 
 @RestController
 public class QueryController {
@@ -23,25 +30,26 @@ public class QueryController {
 
     @GetMapping("rooms")
     public Future<List<RoomSummary>> listRooms() {
-        // TODO: Send a query for this API call.
-        throw new UnsupportedOperationException("Not implemented yet");
+        return queryGateway.query(new AllRoomsQuery(), new MultipleInstancesResponseType<>(RoomSummary.class));
     }
 
     @GetMapping("/rooms/{roomId}/participants")
     public Future<List<String>> participantsInRoom(@PathVariable String roomId) {
-        // TODO: Send a query for this API call.
-        throw new UnsupportedOperationException("Not implemented yet");
+        return queryGateway.query(new RoomParticipantsQuery(roomId), new MultipleInstancesResponseType<>(String.class));
     }
 
     @GetMapping("/rooms/{roomId}/messages")
     public Future<List<ChatMessage>> roomMessages(@PathVariable String roomId) {
-        // TODO: Send a query for this API call.
-        throw new UnsupportedOperationException("Not implemented yet");
+        return queryGateway.query(new RoomMessagesQuery(roomId), new MultipleInstancesResponseType<>(ChatMessage.class));
     }
 
     @GetMapping(value = "/rooms/{roomId}/messages/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ChatMessage> subscribeRoomMessages(@PathVariable String roomId) {
-        // TODO: Send a subscription query for this API call.
-        throw new UnsupportedOperationException("Not implemented yet");
+        RoomMessagesQuery query = new RoomMessagesQuery(roomId);
+        SubscriptionQueryResult<List<ChatMessage>, ChatMessage> result;
+
+        result = queryGateway.subscriptionQuery(query, multipleInstancesOf(ChatMessage.class), instanceOf(ChatMessage.class));
+        Flux<ChatMessage> initialResult = result.initialResult().flatMapMany(Flux::fromIterable);
+        return Flux.concat(initialResult, result.updates());
     }
 }
